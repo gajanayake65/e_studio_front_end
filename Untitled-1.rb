f1ccb85134d66a84c6add2fd7bef6720568cdc3b
gajanayake65 <145248244+gajanayake65@users.noreply.github.com>
2024-04-07 10:56:52 +0530

    Add files via upload

---
+++ Untitled-1.rb
@@ -0,0 +1,29 @@
+<section class="service">
+            <h1 class="headin">our<span>service</span></h1>
+            <div class="service_container">
+                <div class="service_box">
+                <i class="fa-solid fa-upload"></i>
+                <h3> AI Genarator</h3>
+                <p>gjdfsssssssssssssssssssssss
+                    sssssssssssssss</p>
+                    <a href="#" class="btn">Read more</a>
+
+            </div>
+            <div class="service_box">
+                <i class="fa-solid fa-print"></i>
+                <h3> Direct Print</h3>
+                <p>gjdfsssssssssssssssssssssss
+                    sssssssssssssss</p>
+                    <a href="#" class="btn">Read more</a>
+
+            </div>
+            <div class="service_box">
+                <i class="fa-brands fa-envira"></i>
+                <h3> Our Gallery</h3>
+                <p>gjdfsssssssssssssssssssssss
+                    sssssssssssssss</p>
+                    <a href="#" class="btn">Read more</a>
+
+            </div>
+            </div>
+        </section>
